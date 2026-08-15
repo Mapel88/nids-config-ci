7ec5faace4bf9e1fb8d28bdd8b77d240133b097e
Mapel88 <mapelboim@gmail.com>
2026-08-16 01:26:11 +0200

    Update Jenkins job repository URL

--- dockers/jenkins/jobs/nids-pipeline.groovy
+++ dockers/jenkins/jobs/nids-pipeline.groovy
@@ -1,12 +1,12 @@
 // dockers/jenkins/jobs/nids-pipeline.groovy
 pipelineJob('nids-package-build-and-test') {
-    description('Builds and tests NIDS configuration packages for RHEL and Ubuntu.')
+    description('Builds and tests NIDS configuration packages for RHEL-family Linux and Ubuntu.')
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url('https://github.com/mapel88/forescout.git') 
+                        url('https://github.com/Mapel88/nids-config-ci.git')
                     }
                     branch('*/master')
                 }
